2d27df470592efdbb87134994f7826bdb71ba1ef
Sarbast Hesso <sarbast.hesso@gmail.com>
2024-04-17 15:41:56 +0200

    update server

--- server.js
+++ server.js
@@ -2,17 +2,34 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 9999;
-const serverURI = 'http://localhost:' + PORT;
+const PORT = process.env.PORT || 3000;
+// const serverURI = 'http://localhost:' + PORT;
 const mongoURI = process.env.MONGO_URI;
 
+// app.listen(PORT, () => {
+//     console.log('Server running at: ' + serverURI)
+// });
 app.listen(PORT, () => {
-    console.log('Server running at: ' + PORT)
+  console.log("Server is running...");
 });
 
+// mongoose
+// .set('useCreateIndex', true)
+// .connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, () => {console.log('Connected to Mongo-database')})
 mongoose
-.set('useCreateIndex', true)
-.connect(mongoURI, {
+  .set("useCreateIndex", true)
+  .connect(mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {console.log('Connected to Mongo-database')})
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+  
